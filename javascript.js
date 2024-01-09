6024f36a6b235ced33fe3dce1b2f654c21476128
Sergio Eduardo Frioe <sergio.friou@academy.compass.uol>
2024-01-09 18:41:17 +0200

    Verifica se o PIN inserido é válido

--- javascript.js
+++ javascript.js
@@ -7,4 +7,8 @@ function generateRandomPin() {
     // Obtém o PIN inserido pelo usuário
     const userPin = parseInt(document.getElementById('pinInput').value);
   
-  
+  // Verifica se o PIN inserido é válido
+  if (isNaN(userPin) || userPin < 1000 || userPin > 9999) {
+    alert('Por favor, insira um PIN válido de no mínimo 4 dígitos.');
+    return;
+  }
