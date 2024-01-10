3a1991919dd7ad656d8f1c8ecd59b90c580de656
Sergio Eduardo Frioe <sergio.friou@academy.compass.uol>
2024-01-10 20:01:37 +0200

    final

--- javascript.js
+++ javascript.js
@@ -1,32 +1,37 @@
-// Gera um PIN aleatório entre 1000 e 9999
-function generateRandomPin() {
-    return Math.floor(Math.random() * 9000) + 1000;
-  }
-  
-  const expectedPin = generateRandomPin();
-  function checkPin() {
-    // Obtém o PIN inserido pelo usuário
-    const userPin = parseInt(document.getElementById('pinInput').value);
-  
+
+function gerarRandomPin() {
+  return Math.floor(Math.random() * 9000) + 1000;
+}
+
+const esperadoPin = gerarRandomPin();
+
+function checkPin() {
+  // Obtém o PIN inserido pelo usuário
+  const userPin = parseInt(document.getElementById('pinInput').value);
+
   // Verifica se o PIN inserido é válido
   if (isNaN(userPin) || userPin < 1000 || userPin > 9999) {
     alert('Por favor, insira um PIN válido de no mínimo 4 dígitos.');
     return;
   }
-// Compara o PIN inserido com o PIN esperado
-if (userPin === expectedPin) {
+
+  // Compara o PIN inserido com o PIN esperado
+  if (userPin === esperadoPin) {
     document.getElementById('result').textContent = 'Parabéns! PIN correto!';
   } else {
     // Gera uma dica com base na comparação dos PINs
-    let hint = 'PIN incorreto. O próximo valor deve ser ' + (userPin < expectedPin ? 'maior' : 'menor');
+    let hint = 'PIN incorreto. O próximo valor deve ser ' + (userPin < esperadoPin ? 'maior' : 'menor');
 
-        // Adiciona 'Muito' se a diferença for maior que 500
-        if (Math.abs(userPin - expectedPin) > 500) {
-            hint += ' Muito';
-          }
-      
-          document.getElementById('result').textContent = hint;
-          console.log('PIN esperado:', expectedPin);
-        }
-      }
-      
+    // Adiciona 'Muito' se a diferença for maior que 500
+    if (Math.abs(userPin - esperadoPin) > 500) {
+      hint += ' Muito';
+    }
+
+    document.getElementById('result').textContent = hint;
+    console.log('PIN esperado:', esperadoPin);
+  }
+}
+
+
+
+
